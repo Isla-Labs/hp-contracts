ecf91086b5f97bc568a276333a94ce827d19dfce
Isla-Labs <admin@islalabs.co>
2026-07-16 15:49:33 +0100

    Update governance wireframe

---
+++ src/governance/executors-2/class-1/DopplerExecutor.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.34;
+
+/**
+ * After zk proof:
+ * - Deploys Doppler market in bonding
+ * - Adds TokenData, DopplerData, and TournamentData to PlayerSetRegistry
+ * 
+ * After verifying bonding curve status:
+ * - Deploys VaultSet and AdvancedTradeSet
+ */
